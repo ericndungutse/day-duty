c7f198c3968e632a1ed4d27187ead7d848274f60
Eric Ndungutse <dav.ndungutse@gmail.com>
2023-08-24 22:06:19 +0300

    add logout feature

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signup, signin, activateAccount } from "../controllers/auth.controller.js";
+import { signup, signin, activateAccount, logout } from "../controllers/auth.controller.js";
 
 const authRouter = Router()
 
 authRouter.route('/sign-up').post(signup)
 authRouter.route('/sign-in').post(signin)
 authRouter.route('/activate-account/:token/:email').post(activateAccount)
+authRouter.route('/log-out').post(logout)
 
 export default authRouter
